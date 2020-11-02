8e711e3423a4d353c314f5344226f6a47c17ed84
Victor Ambrozi <victorambrozi.dev@gmail.com>
2020-11-01 21:18:39 -0300

    challenger completed

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
+
+server.get("/", (req, res) => 
+    res.sendFile(__dirname + "/views/index.html")
+)
+
+server.listen(5000, () => {
+    console.log("server is ruining")
+})
