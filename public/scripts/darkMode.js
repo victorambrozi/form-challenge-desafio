b10fde9d0763f950814098dec52ed003972cb49a
Victor Ambrozi <victorambrozi.dev@gmail.com>
2020-11-01 21:27:46 -0300

    Revert "challenge completed"
    This reverts commit 742ccc2033fd6f76ef82b086e6fe931cc2438218.

--- public/scripts/darkMode.js
+++ public/scripts/darkMode.js
@@ -1,37 +1,25 @@
-const html = document.querySelector("html")
-const checkbox = document.querySelector("input[name=theme]")
+const body = document.querySelector("body")
+const form = document.querySelector(".form-info form")
+const checkbox = document.querySelector("input[name=theme")
 
-const getStyle = (element, style) =>
-    window
-        .getComputedStyle(element)
-        .getPropertyValue(style)
+removeClasses(body, "dark-body")
+removeClasses(form, "dark-form")
 
+function addClasses(element,style){
+    return element.classList.add(style)
+} 
+function removeClasses(element,style){
+    return element.classList.remove(style)
+} 
 
-const initialColors = {
-    bg: getStyle(html, "--bg-body"),
-    bgForm: getStyle(html, "--color-white"),
-    bgFormInfo: getStyle(html, "--color-blue"),
-    bgbutton: getStyle(html, "--color-green"),
-}
-
-const darkMode = {
-    bg: "#3e3c49",
-    bgForm: "#333",
-    bgFormInfo: "#f1dc1c",
-    bgbutton: "#f1dc1c",
-}
-
-
-const transformKey = key =>  
-    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
-
-
-const changeColors = (colors) => {
-    Object.keys(colors).map(key => 
-        html.style.setProperty(transformKey(key), colors[key])
-    )
-}
 
 checkbox.addEventListener("change", ({ target }) => {
-    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+     if (target.checked) {
+        addClasses(body,"dark-body")
+        addClasses(form, "dark-form")
+     } else {
+        removeClasses(body, "dark-body")
+        removeClasses(form, "dark-form")
+
+     }
 })
